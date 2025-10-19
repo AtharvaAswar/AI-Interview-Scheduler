5f767ee318e1de18aaada53648e3b6fe15f69b86
Atharva Aswar <atharvaaswar02@gmail.com>
2025-10-19 23:20:29 +0530

    signIn Auth

---
+++ app/auth/page.jsx
@@ -0,0 +1,32 @@
+"use client"
+import { Button } from '@/components/ui/button'
+import { supabase } from '@/services/supabaseClient'
+import Image from 'next/image'
+import React from 'react'
+
+function Login() {
+
+  //
+  // Used to signIn with google
+  //
+  const signInWithGoogle = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({ provider: 'google' })
+    if (error) console.log(error.message);
+  }
+
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <div className='flex flex-col items-center border rounded-2xl p-8'>
+        <Image src={'/logo.png'} alt='logo' width={400} height={100} className='w-[180px]' />
+        <div className='flex flex-col items-center'>
+          <Image src={'/login.png'} alt='login' width={600} height={400} className='w-[400px] h-[250px] rounded-2xl' />
+          <h2 className='text-2xl font-bold text-center'>Welcome to AICuriter</h2>
+          <p className='text-gray-500 text-center'>Sign In with Google Authentication</p>
+          <Button className='mt-7 w-full' onClick={signInWithGoogle}> Login with Google</Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
